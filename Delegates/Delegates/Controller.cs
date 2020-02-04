d9117fa8efdb9be3b56dba5dc475a4991ff29eb9
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-04 14:01:49 +0100

    Created Controller class

---
+++ Delegates/Delegates/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Controller
+    {
+        public Controller(string modelName, string manufacturer)
+        {
+            ModelName = modelName;
+            Manufacturer = manufacturer;
+
+            Buttons = new Button[]
+            {
+                new Button("Power"),
+                new Button("Volume Up"),
+                new Button("Volume Down"),
+                new Button("Channel Up"),
+                new Button("Channel Down")
+            };
+        }
+
+        public string ModelName { get; private set; }
+        public string Manufacturer { get; private set; }
+        public Button[] Buttons { get; private set; }
+    }
+}
