08afe4b70ac88b8dc99903c6128fd761a1780562
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-04 09:13:27 +0100

    Completed dictionaries tasks

--- Dictionaries/Dictionaries/Program.cs
+++ Dictionaries/Dictionaries/Program.cs
@@ -10,6 +10,41 @@ namespace Dictionaries
     {
         static void Main(string[] args)
         {
+            var textNumberDictionary = new Dictionary<string, int>()
+            {
+                { "Ti", 10},
+                { "Tyve", 20},
+                { "Tredive", 30},
+                { "Fyrre", 40},
+                { "Halvtreds", 50},
+                { "Tres", 60},
+                { "Halvfjerds", 70},
+                { "Firs", 80},
+                { "Halvfems", 90},
+                { "Hundrede", 100}
+            };
+
+            var floatBooleanDictionary = new Dictionary<float, bool>()
+            {
+                { 1.1F, true},
+                { 1.2F, false},
+                { 1.3F, true},
+                { 1.4F, false},
+                { 1.5F, true},
+                { 1.6F, false},
+                { 1.7F, true},
+                { 1.8F, false},
+                { 1.9F, true},
+                { 2.0F, false},
+            };
+
+            var h2ObjectOrientedProg = new Dictionary<string, int>()
+            {
+                { "Andreas", 25},
+                { "Aputsiaq", 27},
+                { "Kasper", 20},
+                { "Mads", 29}
+            };
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Færdige opgaver:
+Dictionaries
