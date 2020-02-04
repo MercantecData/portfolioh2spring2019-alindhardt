95154554db9c1780607c19563f21ab72b125a084
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-04 14:08:42 +0100

    Created Television class

---
+++ Delegates/Delegates/Television.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Television
+    {
+        public Television(string modelName, string manufacturer)
+        {
+            ModelName = modelName;
+            Manufacturer = manufacturer;
+        }
+
+        public string ModelName { get; private set; }
+        public string Manufacturer { get; private set; }
+        public PowerState PowerState { get; private set; }
+    }
+}
