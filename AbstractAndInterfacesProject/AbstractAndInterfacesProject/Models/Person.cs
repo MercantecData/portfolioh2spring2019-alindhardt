c527d052ebf5a6e3d32deb516a87a0ca618c14e7
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 13:41:41 +0100

    Create Person.cs and used IComparable

---
+++ AbstractAndInterfacesProject/AbstractAndInterfacesProject/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAndInterfacesProject.Models
+{
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool IsMale { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            ////Sort by Age
+            //return Age.CompareTo(other.Age);
+
+            //Sort Name, Age, Sex
+            var result = Name.CompareTo(other.Name);
+            if (result == 0) result = Age.CompareTo(other.Age);
+            if (result == 0) result = IsMale.CompareTo(other.IsMale);
+
+            return result;
+        }
+    }
+}
--- AbstractAndInterfacesProject/AbstractAndInterfacesProject/Program.cs
+++ AbstractAndInterfacesProject/AbstractAndInterfacesProject/Program.cs
@@ -17,6 +17,29 @@ namespace AbstractAndInterfacesProject
 
             //Abstract method from ICopyAndPastetable.cs
             triangle.Copy();
+
+
+            //IComparable Test
+            var people = new List<Models.Person>
+            {
+                new Models.Person{Name = "Bo", Age = 51, IsMale = true},
+                new Models.Person{Name = "Frank", Age = 8, IsMale = true},
+                new Models.Person{Name = "Marie", Age = 33, IsMale = false},
+                new Models.Person{Name = "Jens", Age = 52, IsMale = true},
+                new Models.Person{Name = "Mathilde", Age = 70, IsMale = false},
+                new Models.Person{Name = "Anders", Age = 2, IsMale = true},
+                new Models.Person{Name = "Sven", Age = 22, IsMale = true},
+                new Models.Person{Name = "Eva", Age = 23, IsMale = false},
+                new Models.Person{Name = "Emilie", Age = 5, IsMale = false},
+                new Models.Person{Name = "Ib", Age = 45, IsMale = true},
+            };
+
+            people.Sort();
+
+            foreach (var person in people)
+            {
+                Console.WriteLine("Age: {0}, Sex: {1}, Name: {2}", person.Age, person.IsMale ? "Male" : "Female", person.Name);
+            }
         }
     }
 }
