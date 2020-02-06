1e0f9022806284e25a128771e0d485ec9cf36639
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 20:05:03 +0100

    Create Tablet.cs

---
+++ LibrariesProject/LibrariesProject/Tablet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibrariesProject
+{
+    public class Tablet : Computer
+    {
+        public Tablet(string manufacturer, float cpuMhz, int cpuCores) : base(manufacturer, cpuMhz, cpuCores)
+        {
+
+        }
+
+        public bool Battery { get; private set; }
+    }
+}
