d9dae7ccdf84377730b3e549e65407c35fdba337
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 10:35:42 +0100

    Created Laptop.cs in Models\

---
+++ InheritanceProject1/InheritanceProject1/Models/Laptop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProject1.Models
+{
+    public class Laptop : Computer
+    {
+        public Laptop(string manufacturer, int cpuMhz, int cpuCores) : base(manufacturer, cpuMhz, cpuCores)
+        {
+
+        }
+
+        public bool Battery { get; private set; }
+        public bool LidOpen { get; private set; }
+
+        public void ChangeLidState()
+        {
+            LidOpen = LidOpen ? false : true;
+        }
+    }
+}
