1dcaa0a6add1437a745a5f1db92d04ab8eecff8e
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 20:04:10 +0100

    Create SmartWatch.cs

---
+++ LibrariesProject/LibrariesProject/SmartWatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibrariesProject
+{
+    public class SmartWatch : Computer
+    {
+        public SmartWatch(string manufacturer, float cpuMhz, int cpuCores) : base(manufacturer, cpuMhz, cpuCores)
+        {
+
+        }
+
+        public bool Battery { get; private set; }
+
+        public void MeasurePulse()
+        {
+            Console.WriteLine("Measuring pulse...");
+            Console.WriteLine("No pulse found.");
+        }
+    }
+}
