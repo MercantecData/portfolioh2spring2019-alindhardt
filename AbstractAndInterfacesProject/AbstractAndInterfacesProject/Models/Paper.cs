058d285b9626e257034a8ace632b05c879006021
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 12:53:32 +0100

    Created Paper.cs that uses ICopyPastetable.cs

---
+++ AbstractAndInterfacesProject/AbstractAndInterfacesProject/Models/Paper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAndInterfacesProject.Models
+{
+    public class Paper : ICopyAndPastetable
+    {
+        public string Text { get; set; }
+
+        public void Copy()
+        {
+            System.Windows.Forms.Clipboard.SetText(Text);
+        }
+
+        public void Cut()
+        {
+            System.Windows.Forms.Clipboard.SetText(Text);
+            Text = string.Empty;
+        }
+
+        public void Paste()
+        {
+            Text = System.Windows.Forms.Clipboard.GetText();
+        }
+    }
+}
