309261079e58c5e353ad37b93c1d7f68b2f5d359
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 10:45:18 +0100

    Created Battery.cs in Models\

---
+++ InheritanceProject1/InheritanceProject1/Models/Tablet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProject1.Models
+{
+    public class Tablet : Computer
+    {
+        public Tablet(string manufacturer, int cpuMhz, int cpuCores) : base(manufacturer, cpuMhz, cpuCores)
+        {
+
+        }
+
+        public bool Battery { get; private set; }
+    }
+}
