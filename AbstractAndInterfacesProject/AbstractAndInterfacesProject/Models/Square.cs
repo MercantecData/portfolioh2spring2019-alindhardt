33f6a239143c3f02f442bf90801a17f378f5fd0e
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 12:13:49 +0100

    Created Square.cs in Models\

---
+++ AbstractAndInterfacesProject/AbstractAndInterfacesProject/Models/Square.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAndInterfacesProject.Models
+{
+    public class Square : Shape
+    {
+        public override void Draw()
+        {
+            var text = @"
+ _
+|_|
+";
+            Console.WriteLine(text);
+        }
+    }
+}
