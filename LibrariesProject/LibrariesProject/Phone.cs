92587534bb0e5d363ef6412733b4335eb90ba3ba
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 20:03:27 +0100

    Create Phone.cs

---
+++ LibrariesProject/LibrariesProject/Phone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibrariesProject
+{
+    public class Phone : Computer
+    {
+        public Phone(string manufacturer, float cpuMhz, int cpuCores) : base(manufacturer, cpuMhz, cpuCores)
+        {
+
+        }
+
+        public bool Battery { get; set; }
+
+        public void Call(string phoneNumber)
+        {
+            Console.WriteLine("Calling: {0}", phoneNumber);
+        }
+    }
+}
