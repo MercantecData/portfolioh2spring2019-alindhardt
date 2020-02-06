72efd6f36768b387f7e99acbcbe196178d65a7fb
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 12:11:30 +0100

    Created Circle.cs in Models\

---
+++ AbstractAndInterfacesProject/AbstractAndInterfacesProject/Models/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAndInterfacesProject.Models
+{
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            var text = @"
+        , - ~ ~ ~ - ,
+     , '               ' ,
+   ,                       ,
+  ,                         ,
+ ,                           ,
+ ,                           ,
+ ,                           ,
+  ,                         ,
+   ,                       ,
+     ,                  , '
+       ' - , _ _ _ ,  '
+
+";
+            Console.WriteLine(text);
+        }
+    }
+}
