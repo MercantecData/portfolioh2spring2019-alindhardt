6b375c22ccc524d1ca3ed6d522d9a96b3a30aa8b
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 10:38:35 +0100

    Created Phone.cs in Models\

---
+++ InheritanceProject1/InheritanceProject1/Models/Phone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProject1.Models
+{
+    public class Phone : Computer
+    {
+        public Phone(string manufacturer, int cpuMhz, int cpuCores) : base(manufacturer, cpuMhz, cpuCores)
+        {
+
+        }
+
+        public bool Battery { get; set; }
+
+        public void Call(string phoneNumber)
+        {
+            Console.WriteLine("Calling: {0}", phoneNumber);
+        }
+    }
+}
