1551222e6524a660ca949de8c65e0a3b79b58d69
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 08:08:38 +0100

    Created Motherboard.cs to Models

---
+++ InheritanceProject1/InheritanceProject1/Models/Motherboard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProject1.Models
+{
+    public class Motherboard
+    {
+        public string Type { get; private set; } = "ATX";
+        public string[] IO { get; private set; } = new string[] { "USB1", "USB2"};
+
+        public void Boot()
+        {
+            Console.WriteLine("I AM BOOTING!!!");
+        }
+    }
+}
