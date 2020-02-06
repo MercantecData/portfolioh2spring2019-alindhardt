cb21881eb362f5481a151d6a8e5224558f228c77
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 20:02:40 +0100

    Create Laptop.cs

---
+++ LibrariesProject/LibrariesProject/Laptop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibrariesProject
+{
+    public class Laptop : Computer
+    {
+        public Laptop(string manufacturer, float cpuMhz, int cpuCores) : base(manufacturer, cpuMhz, cpuCores)
+        {
+
+        }
+
+        public bool Battery { get; private set; }
+        public bool LidOpen { get; private set; }
+
+        public void ChangeLidState()
+        {
+            LidOpen = LidOpen ? false : true;
+        }
+    }
+}
