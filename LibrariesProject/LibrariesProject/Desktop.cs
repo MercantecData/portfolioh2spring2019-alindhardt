db895fb69a06d22fdde539088c52de887f32f202
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 20:01:39 +0100

    Create Desktop.cs

---
+++ LibrariesProject/LibrariesProject/Desktop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibrariesProject
+{
+    public class Desktop : Computer
+    {
+        /// <summary>
+        /// Initialize Desktop with manufacturer and specific cpu Mhz and cores.
+        /// </summary>
+        /// <param name="manufacturer"></param>
+        /// <param name="cpuMhz"></param>
+        /// <param name="cpuCores"></param>
+        public Desktop(string manufacturer, float cpuMhz, int cpuCores) : base(manufacturer, cpuMhz, cpuCores)
+        {
+
+        }
+
+        public bool HasPsu { get; private set; }
+    }
+}
