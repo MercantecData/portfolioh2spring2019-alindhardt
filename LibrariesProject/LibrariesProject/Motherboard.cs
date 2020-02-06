f28c42958f4d4df21391ec451f9673e2281bdb27
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 19:58:37 +0100

    Created LibrariesProject

---
+++ LibrariesProject/LibrariesProject/Motherboard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LibrariesProject
+{
+    public class Motherboard
+    {
+        public string Type { get; private set; } = "ATX";
+        public string[] IO { get; private set; } = new string[] { "USB1", "USB2" };
+
+        public void Boot()
+        {
+            Console.WriteLine("I AM BOOTING!!!");
+        }
+    }
+}
