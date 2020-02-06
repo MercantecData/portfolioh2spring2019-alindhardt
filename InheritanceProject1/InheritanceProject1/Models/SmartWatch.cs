9afb132b43f608835ba510e930c1d4a07160589d
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 10:43:06 +0100

    Created SmartWatch.cs in Models\

---
+++ InheritanceProject1/InheritanceProject1/Models/SmartWatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProject1.Models
+{
+    public class SmartWatch : Computer
+    {
+        public SmartWatch(string manufacturer, int cpuMhz, int cpuCores) : base(manufacturer, cpuMhz, cpuCores)
+        {
+
+        }
+
+        public bool Battery { get; private set; }
+
+        public void MeasurePulse()
+        {
+            Console.WriteLine("Measuring pulse...");
+            Console.WriteLine("No pulse found.");
+        }
+    }
+}
