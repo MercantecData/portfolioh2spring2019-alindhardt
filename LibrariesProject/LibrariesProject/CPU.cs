eced34c93e0a9a610d3c62d7040ca6c0ea6ced43
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 19:59:33 +0100

    Create CPU.cs

---
+++ LibrariesProject/LibrariesProject/CPU.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibrariesProject
+{
+    public class CPU
+    {
+        public CPU(float mhz, int cores)
+        {
+            Mhz = mhz;
+            Cores = cores;
+        }
+
+        public float Mhz { get; private set; }
+        public int Cores { get; private set; }
+    }
+}
