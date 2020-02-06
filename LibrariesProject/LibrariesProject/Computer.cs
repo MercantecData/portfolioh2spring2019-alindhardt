3c9f45253a858b14190f255feb4ecbdd0242fb60
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 20:00:37 +0100

    Create Computer.cs

---
+++ LibrariesProject/LibrariesProject/Computer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibrariesProject
+{
+    public class Computer
+    {
+        /// <summary>
+        /// Initialize Computer with manufacturer and specific cpu Mhz and cores.
+        /// </summary>
+        /// <param name="manufacturer">The name of the computer manufacturer</param>
+        /// <param name="cpuMhz">The number of Mhz in the computer's CPU</param>
+        /// <param name="cpuCores">The number of cores in the computer's CPU</param>
+        public Computer(string manufacturer, float cpuMhz, int cpuCores)
+        {
+            Manufacturer = manufacturer;
+            CPU = new CPU(cpuMhz, cpuCores);
+        }
+
+        public string Manufacturer { get; private set; }
+        public Motherboard Motherboard { get; private set; }
+        public CPU CPU { get; private set; }
+        public bool PoweredOn { get; private set; }
+
+        public void ChangePowerState()
+        {
+            PoweredOn = PoweredOn ? false : true;
+        }
+    }
+}
