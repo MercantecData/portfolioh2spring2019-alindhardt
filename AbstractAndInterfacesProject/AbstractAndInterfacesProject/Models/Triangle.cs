f87846d19e46c20f07373c39d268cd701b77ecdb
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 12:08:08 +0100

    Created Triangle.cs in Models\

---
+++ AbstractAndInterfacesProject/AbstractAndInterfacesProject/Models/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAndInterfacesProject.Models
+{
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            var text = @"
+ _        _                   _      
+| |      (_)                 | |     
+| |_ _ __ _  __ _ _ __   __ _| | ___ 
+| __| '__| |/ _` | '_ \ / _` | |/ _ \
+| |_| |  | | (_| | | | | (_| | |  __/
+ \__|_|  |_|\__,_|_| |_|\__, |_|\___|
+                         __/ |       
+                        |___/       
+";
+            Console.WriteLine(text);
+        }
+    }
+}
