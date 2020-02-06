af71d128ca80eeea052d575919db305e26f93f36
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 09:27:55 +0100

    Created Computer.cs in Models\

---
+++ InheritanceProject1/InheritanceProject1/Models/Computer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProject1.Models
+{
+    public class Computer
+    {
+        /// <summary>
+        /// Initialize Computer with manufacturer and specific cpu Mhz and cores.
+        /// </summary>
+        /// <param name="manufacturer">The name of the computer manufacturer</param>
+        /// <param name="cpuMhz">The number of Mhz in the computer's CPU</param>
+        /// <param name="cpuCores">The number of cores in the computer's CPU</param>
+        public Computer(string manufacturer, int cpuMhz, int cpuCores)
+        {
+            Manufacturer = manufacturer;
+            CPU = new CPU(cpuMhz, cpuCores);
+        }
+
+        public string Manufacturer { get; private set; }
+        public Motherboard Motherboard { get; private set; }
+        public CPU CPU { get; private set; }
+        public bool PoweredOn { get; private set; }
+    }
+}
