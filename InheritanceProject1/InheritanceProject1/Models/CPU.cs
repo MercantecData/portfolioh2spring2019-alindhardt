c3a16153e8a00ff9fadf2fe5bfb4e6dbadf1ba76
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 09:01:33 +0100

    Created CPU.cs in Models\

---
+++ InheritanceProject1/InheritanceProject1/Models/CPU.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProject1.Models
+{
+    public class CPU
+    {
+        public CPU(int mhz, int cores)
+        {
+            Mhz = mhz;
+            Cores = cores;
+        }
+
+        public int Mhz { get; private set; }
+        public int Cores { get; private set; }
+    }
+}
