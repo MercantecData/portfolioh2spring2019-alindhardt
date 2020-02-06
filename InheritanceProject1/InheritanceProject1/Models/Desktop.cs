5df0a7eb792dc56027dda2affca7374b785e4e75
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 09:59:11 +0100

    Created Desktop.cs in Models\

---
+++ InheritanceProject1/InheritanceProject1/Models/Desktop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProject1.Models
+{
+    public class Desktop : Computer
+    {
+        /// <summary>
+        /// Initialize Desktop with manufacturer and specific cpu Mhz and cores.
+        /// </summary>
+        /// <param name="manufacturer"></param>
+        /// <param name="cpuMhz"></param>
+        /// <param name="cpuCores"></param>
+        public Desktop(string manufacturer, int cpuMhz, int cpuCores) : base(manufacturer, cpuMhz, cpuCores)
+        {
+
+        }
+
+        public bool HasPsu { get; private set; }
+    }
+}
