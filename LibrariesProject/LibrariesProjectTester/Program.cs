d93d379d698954d1ea2ea20aeb3b57a6f13a97b4
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-06 20:12:51 +0100

    Created console application project that uses library

---
+++ LibrariesProject/LibrariesProjectTester/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LibrariesProjectTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myLaptop = new LibrariesProject.Laptop("HP", 2400, 4);
+            if (!myLaptop.LidOpen)
+            {
+                Console.WriteLine("Opening laptop lid!!!");
+                myLaptop.ChangeLidState();
+            }
+        }
+    }
+}
