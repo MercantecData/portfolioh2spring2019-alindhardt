366df17a2791ae918a072f4488d668f938ed4972
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-07 08:18:19 +0100

    Create User.cs

---
+++ Opsumering/Opsumering/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opsumering
+{
+    public class User
+    {
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+        public Job Job { get; private set; }
+
+        public bool IsAdmin()
+        {
+            return false;
+        }
+    }
+}
