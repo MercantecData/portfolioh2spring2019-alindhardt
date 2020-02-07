618901836624d6f2484a63f1eeda7990769db1d4
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-07 08:11:58 +0100

    Create HourlyPaidJob.cs

---
+++ Opsumering/Opsumering/HourlyPaidJob.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opsumering
+{
+    public class HourlyPaidJob : Job
+    {
+        private int HourlyPay { get; set; }
+
+        public HourlyPaidJob(int hourlyPay)
+        {
+            HourlyPay = hourlyPay;
+        }
+
+        public override int GetMonthlyPay()
+        {
+            return HourlyPay;
+        }
+    }
+}
