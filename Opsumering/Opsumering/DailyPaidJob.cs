cbbd5b34e92238f267bb1420a678e788fd524cb5
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-07 08:16:07 +0100

    Create DailyPaidJob.cs

---
+++ Opsumering/Opsumering/DailyPaidJob.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opsumering
+{
+    public class DailyPaidJob : Job
+    {
+        public int DailyPay { get; set; }
+
+        public DailyPaidJob(int dailyPay)
+        {
+            DailyPay = dailyPay;
+        }
+
+        public override int GetMonthlyPay()
+        {
+            return DailyPay * 30;
+        }
+    }
+}
