65e6db754a2d41f3e185eb8352872716e038bbce
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-07 08:35:12 +0100

    Create Registry.cs

---
+++ Opsumering/Opsumering/Registry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Opsumering
+{
+    public class Registry
+    {
+        public List<User> Users { get; private set; } = new List<User>();
+        public List<Admin> Admins { get; private set; } = new List<Admin>();
+
+        public void AddNewUser(User user)
+        {
+            Users.Add(user);
+            Console.WriteLine("{0} added to list of users", user.Login);
+        }
+
+        public bool Login(string login, string password)
+        {
+            var userWithLoginAndPassword = Users
+                .Where(u => u.Login == login)
+                .Where(u => u.Password == password)
+                .FirstOrDefault();
+
+            return (userWithLoginAndPassword != null) ? true : false;
+        }
+    }
+}
