6e81bb49b52c9a9aaeb5d327a3e61d73e4ad0ae4
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-10 10:22:15 +0100

    Update Program.cs

--- Opsumering/OpsumeringTest/Program.cs
+++ Opsumering/OpsumeringTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace OpsumeringTest
 {
@@ -6,7 +7,20 @@ namespace OpsumeringTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var registry = new Opsumering.Registry();
+            registry.Users.AddRange(new Opsumering.User[] 
+            {
+                new Opsumering.User("User1", "Password1", null, () => registry.Admins),
+                new Opsumering.User("User2", "Password1", null, () => registry.Admins),
+                new Opsumering.User("User3", "Password1", null, () => registry.Admins),
+                new Opsumering.User("User4", "Password1", null, () => registry.Admins),
+                new Opsumering.User("User5", "Password1", null, () => registry.Admins)
+            });
+
+            var admin = new Opsumering.Admin("Admin1", "Password1", null, () => registry.Admins);
+            registry.Admins.Add(admin);
+
+            admin.ChangePassword(registry.Users.Single(x => x.Login == "User1"), "Password2");
         }
     }
 }
