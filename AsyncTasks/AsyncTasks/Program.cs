3dbb9ef96989094c3aa2e4fec351155ff18f928c
alindhardt <49632707+alindhardt@users.noreply.github.com>
2020-02-10 10:45:06 +0100

    Created AsyncTasks console app

---
+++ AsyncTasks/AsyncTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
